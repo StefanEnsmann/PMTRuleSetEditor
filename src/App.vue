25c1f8272ab5d0a8c5eb304125bd3404f898359c
Stefan Ensmann <stefan@ensmann.de>
2022-03-01 22:24:22 +0100

    added basic navigation

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,167 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <n-layout has-sider>
+      <n-layout-sider
+        bordered
+        collapse-mode="width"
+        :collapsed-width="64"
+        :width="240"
+        :collapsed="collapsed"
+        show-trigger
+        @collapse="collapsed = true"
+        @expand="collapsed = false"
+      >
+        <n-menu 
+          v-model:value="activeKey"
+          :collapsed="collapsed"
+          :collapsed-width="64"
+          :collapsed-icon-size="22"
+          :options="menuOptions" 
+        />
+      </n-layout-sider>
+      <n-layout>
+        <n-page-header>
+          <n-grid :cols="4">
+            <n-gi><n-statistic label="Checks" value="123">
+              <template #prefix>
+                <n-icon>
+                  <PlaylistAddCheckRound />
+                </n-icon>
+              </template>
+              </n-statistic>
+            </n-gi>
+            <n-gi><n-statistic label="Story Items" value="123" /></n-gi>
+            <n-gi><n-statistic label="Pokedex" value="123" /></n-gi>
+            <n-gi><n-statistic label="Maps" value="123" /></n-gi>
+          </n-grid>
+        </n-page-header>
+        <RouterView></RouterView>
+      </n-layout>
+    </n-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { h, ref } from "vue"
+import { NIcon, NLayout, NMenu, NLayoutSider, NPageHeader, NGrid, NStatistic, NGi } from "naive-ui"
+import { RouterLink } from "vue-router"
+import {
+  HomeRound,
+  InfoRound,
+  PlaylistAddCheckRound,
+  BackpackRound,
+  CatchingPokemonRound,
+  MapRound,
+  HelpOutlineRound
+} from "@vicons/material"
+
+function renderIcon(icon) {
+  return() => h(NIcon, null, { default: () => h(icon) })
+}
+
+const menuOptions = [
+  {
+    label: () => h(RouterLink, {
+      to: {
+        path: "/"
+      }
+    }, { default: () => "Home" }),
+    key: "go-home",
+    icon: renderIcon(HomeRound)
+  },
+  {
+    key: "home-divider",
+    type: "divider"
+  },
+  {
+    label: () => h(RouterLink, {
+      to: {
+        path: "/information"
+      }
+    }, { default: () => "Information" }),
+    key: "go-to-information",
+    icon: renderIcon(InfoRound)
+  },
+  {
+    label: () => h(RouterLink, {
+      to: {
+        path: "/locations"
+      }
+    }, { default: () => "Locations" }),
+    key: "go-to-locations",
+    icon: renderIcon(PlaylistAddCheckRound)
+  },
+  {
+    label: () => h(RouterLink, {
+      to: {
+        path: "/story-items"
+      }
+    }, { default: () => "Story Items" }),
+    key: "go-to-story-items",
+    icon: renderIcon(BackpackRound)
+  },
+  {
+    label: () => h(RouterLink, {
+      to: {
+        path: "/pokedex"
+      }
+    }, { default: () => "Pokédex" }),
+    key: "go-to-pokedex",
+    icon: renderIcon(CatchingPokemonRound)
+  },
+  {
+    label: () => h(RouterLink, {
+      to: {
+        path: "/maps"
+      }
+    }, { default: () => "Maps" }),
+    key: "go-to-maps",
+    icon: renderIcon(MapRound)
+  },
+  {
+    key: "about-divider",
+    type: "divider"
+  },
+  {
+    label: () => h(RouterLink, {
+      to: {
+        path: "/about"
+      }
+    }, { default: () => "About" }),
+    key: "go-to-about",
+    icon: renderIcon(HelpOutlineRound)
+  }
+]
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PlaylistAddCheckRound,
+    NIcon,
+    NLayout,
+    NMenu,
+    NLayoutSider,
+    NPageHeader,
+    NGrid,
+    NGi,
+    NStatistic
+  },
+  setup() {
+    return {
+      activeKey: ref("go-home"),
+      collapsed: ref(true),
+      menuOptions
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
+.n-layout {
+  height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import HomeView from './components/views/HomeView'
+import InformationView from './components/views/InformationView'
+import LocationsView from './components/views/LocationsView'
+import StoryItemsView from './components/views/StoryItemsView'
+import PokedexView from './components/views/PokedexView'
+import MapsView from './components/views/MapsView'
+import AboutView from './components/views/AboutView'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: HomeView },
+    { path: '/information', component: InformationView },
+    { path: '/locations', component: LocationsView },
+    { path: '/story-items', component: StoryItemsView },
+    { path: '/pokedex', component: PokedexView },
+    { path: '/maps', component: MapsView },
+    { path: '/about', component: AboutView }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
