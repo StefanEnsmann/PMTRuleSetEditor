3247032ccf521eb4c63c0fb9fd384d3c8de85b71
Stefan Ensmann <stefan@ensmann.de>
2022-03-13 20:12:57 +0100

    added stores, router and pokemon card

--- src/components/views/PokedexView.vue
+++ src/components/views/PokedexView.vue
@@ -1,16 +1,53 @@
 <template>
   <div class="pokedex-view">
-    Pokedex View
+    <n-grid cols="3 s:4 m:5 l:6 xl:7 2xl:8" responsive="screen">
+      <n-grid-item>
+        <PokemonCard
+          id=001
+          name="Bulbasaur"
+          imgSrc="https://pkmntracker.ensmann.de/img/pkmn/0001_bulbasaur.png"
+          typeA="grass"
+          typeB="poison"
+        />
+      </n-grid-item>
+      <n-grid-item>
+        <PokemonCard
+          id=002
+          name="Ivysaur"
+          imgSrc="https://pkmntracker.ensmann.de/img/pkmn/0002_ivysaur.png"
+          typeA="grass"
+          typeB="poison"
+        />
+      </n-grid-item>
+      <n-grid-item>
+        <PokemonCard
+          id=003
+          name="Venusaur"
+          imgSrc="https://pkmntracker.ensmann.de/img/pkmn/0003_venusaur.png"
+          typeA="grass"
+          typeB="poison"
+        />
+      </n-grid-item>
+    </n-grid>
   </div>
 </template>
 
 <script lang="ts">
-export default {
+import { defineComponent } from 'vue'
+import { NGrid, NGridItem } from 'naive-ui'
+import PokemonCard from '../pokedex/PokemonCard.vue'
+
+export default defineComponent({
   name: 'PokedexView',
+  components: {
+    PokemonCard,
+    NGrid,
+    NGridItem
+  },
   props: {
     msg: String
   }
-}
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
